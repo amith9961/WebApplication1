fb83f5e2bb378aba1789b8d402a827566d591ae8
amith.v <amith.v@PIC4398>
2022-12-15 13:24:46 +0530

    update dapper

--- WebApplication1/Data/Get/GetDataRepository.cs
+++ WebApplication1/Data/Get/GetDataRepository.cs
@@ -1,26 +1,26 @@
-﻿//using ProductionForum.DataAccess.PostgreSQL.ConnectionFactory;
-//using ProductionForum.DataAccess.PostgreSQL.Repository;
-//using System.Collections.Generic;
-//using System.Threading.Tasks;
-//using WebApplication1;
-//using WebApplication1.Models;
+﻿using ProductionForum.DataAccess.PostgreSQL.ConnectionFactory;
+using ProductionForum.DataAccess.PostgreSQL.Repository;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApplication1;
+using WebApplication1.Models;
 
-//public interface IGetRepository
-//{
-//    Task<IEnumerable<Article>> GetArticles();
-//}
-//public class GetRepository : BaseRepository, IGetRepository
-//{
-//    #region CONSTRUCTOR
+public interface IGetRepository
+{
+    Task<IEnumerable<Article>> GetArticles();
+}
+public class GetRepository : BaseRepository, IGetRepository
+{
+    #region CONSTRUCTOR
 
-//    public GetRepository(IPostgreSqlDatabaseConnectionFactory connectionFactory) : base(connectionFactory)
-//    {
+    public GetRepository(IPostgreSqlDatabaseConnectionFactory connectionFactory) : base(connectionFactory)
+    {
 
-//    }
-//    public async Task<IEnumerable<Article>> GetArticles()
-//    {
-//        var result = await this.QueryAsync("SELECT * FROM article");
-//        return result;
-//    }
-//    #endregion
-//}
+    }
+    public async Task<IEnumerable<Article>> GetArticles()
+    {
+        var result = await this.QueryAsync("SELECT * FROM article");
+        return result;
+    }
+    #endregion
+}
